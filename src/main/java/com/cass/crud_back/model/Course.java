6bae2bba6db8326d5ddb19c1ea26eee2119828ef
Cass <cassianoleaoramos@gmail.com>
2023-10-21 12:24:38 -0300

    adding soft delete

--- src/main/java/com/cass/crud_back/model/Course.java
+++ src/main/java/com/cass/crud_back/model/Course.java
@@ -6,10 +6,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+//The syntax "id = ?" represents the id parameter passed in the delete method on controller within the controller class
+
+@Where(clause = "status = 'Active'")
+//filter using hibernate properties
 public class Course {
 
     @Id
@@ -28,5 +35,11 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 
 }
