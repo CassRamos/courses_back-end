41c4f044d54eb4e2eea2d671e3bbb190b2663bf1
Cass <cassianoleaoramos@gmail.com>
2023-11-02 12:07:49 -0300

    implement Java Bean Validations for enums

--- src/main/java/com/cass/crud_back/dto/CourseDTO.java
+++ src/main/java/com/cass/crud_back/dto/CourseDTO.java
@@ -1,5 +1,7 @@
 package com.cass.crud_back.dto;
 
+import com.cass.crud_back.enums.Category;
+import com.cass.crud_back.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -14,7 +16,7 @@ public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @NotBlank @Length(min = 5, max = 40) String name,
         //status is not a data that should be exposed in a DTO
-        @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+        @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons
 ) {
 }
