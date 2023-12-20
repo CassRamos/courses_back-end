4e89f6ce6a4e415555092cc0a636c70633837896
Cass <cassianoleaoramos@gmail.com>
2023-12-20 20:06:23 -0300

    Add PostgreSQL connection configuration and Docker Compose file

--- src/main/java/com/cass/crud_back/CrudBackApplication.java
+++ src/main/java/com/cass/crud_back/CrudBackApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Locale;
 
@@ -19,6 +20,7 @@ public class CrudBackApplication {
     }
 
     @Bean
+    @Profile("dev")
     CommandLineRunner initDataBase(CourseRepo courseRepo) {
         return args -> {
             courseRepo.deleteAll();
